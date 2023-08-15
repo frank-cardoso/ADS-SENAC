b707f95a50691cc61d7479194522d91a56d45129
frank-cardoso <114771200+frank-cardoso@users.noreply.github.com>
2023-08-15 11:00:06 -0300

    Add files via upload

--- POO/08-08-23/EX1.java
+++ POO/08-08-23/EX1.java
@@ -1,1 +1,17 @@
+public class EX1 {
+//    Atividade 1
+    public static void main(String[] args) {
+        byte percentual = 55;
+        double maquina = 99999999;
+        int codigo = 1222222;
+        char nota = 'S';
+        boolean carteira = true;
+        float suprimentos = 1.400000000000000f;
 
+        double resultado = 10.00 + 10 + 4;
+        System.out.println(resultado);
+
+        boolean expressao = resultado !=24;
+        System.out.println(expressao);
+    }
+}
